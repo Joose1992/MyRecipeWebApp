070f3f0bd6def7e112e1ef6e44e0098451cb1aec
Jose Montanez <josemontanez1992@gmail.com>
2023-10-17 20:32:29 -0600

    Added second MVC Version

---
+++ MyRecipeWebAppSecondVersion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyRecipeWebAppSecondVersion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

bd3cf1e1bef8b0a06bc26df8fd16f95b93511770
Jose Montanez <josemontanez1992@gmail.com>
2023-10-18 13:38:47 -0600

    added Second version view model class

---
+++ MyRecipeWebAppSecondVersion/Models/SecondRecipeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RecipesWebApp.Data.Models;
+
+namespace MyRecipeWebAppSecondVersion.Models
+{
+    public class SecondRecipeViewModel
+    {
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; }
+        public string MealType { get; set; }
+        public string MainIngredients { get; set; }
+        public string PrepTime { get; set; }
+        
+        public bool IsSuccess { get; set; }
+        public string AccionMessage { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public Recipes CurrentRecipe { get; set; }
+        public List<Recipes> RecipeList { get; set; }
+    }
+}
